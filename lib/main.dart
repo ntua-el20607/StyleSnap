3fc2009f749b33946fd3996acfc5f013b639d90c
christosmais01 <christosmais01@gmail.com>
2024-01-12 15:08:22 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 //import 'package:stylesnap/screens/start.dart';
 //import 'package:stylesnap/screens/login.dart';
 //import 'package:stylesnap/screens/sign_up.dart'; // Ensure this path is correct
 import 'package:stylesnap/screens/changephoto.dart';
+import 'package:stylesnap/screens/sign_up.dart';
 //import 'package:stylesnap/screens/Edit_Profile.dart';
 //import 'package:stylesnap/screens/scanQR.dart';
 //import 'package:stylesnap/screens/QRmenu.dart';
 //import 'package:stylesnap/screens/Profile.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Myapp());
 }
 
@@ -23,7 +30,7 @@ class Myapp extends StatelessWidget {
         // You can define the global theme of your app here (optional)
         primarySwatch: Colors.blue,
       ),
-      home: ChangePhotoScreen(), // Set the home to your Login widget
+      home: Signup(), // Set the home to your Login widget
     );
   }
 }
