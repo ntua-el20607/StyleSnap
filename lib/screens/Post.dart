5e7a9de8bba9ba79322a0607ede76b7a087ce57c
ntua-el20607 <el20607@mail.ntua.gr>
2024-01-14 12:38:56 +0200

    post -ruklas

---
+++ lib/screens/Post.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class Post extends StatefulWidget {
+  const Post({super.key});
+  @override
+  _Post createState() => _Post();
+}
+
+class _Post extends State<Post> {
+  CameraController? _controller;
+  List<CameraDescription>? _cameras;
+  bool _isRearCameraSelected = true;
+  bool _isFlashOn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  Future<void> _initCamera() async {
+    _cameras = await availableCameras();
+    if (_cameras != null && _cameras!.isNotEmpty) {
+      _controller = CameraController(
+          _isRearCameraSelected ? _cameras![0] : _cameras![1],
+          ResolutionPreset.max,
+          enableAudio: false,
+          imageFormatGroup: ImageFormatGroup.jpeg);
+      await _controller!.initialize();
+      if (!mounted) return;
+      setState(() {});
+    }
+  }
+
+  Future<void> _toggleCamera() async {
+    if (_cameras != null && _cameras!.length > 1) {
+      _isRearCameraSelected = !_isRearCameraSelected;
+      await _initCamera();
+    }
+  }
+
+  Future<void> _toggleFlash() async {
+    if (_controller != null) {
+      _isFlashOn = !_isFlashOn;
+      await _controller!
+          .setFlashMode(_isFlashOn ? FlashMode.torch : FlashMode.off);
+      setState(() {});
+    }
+  }
+
+  Future<void> _takePicture() async {
+    if (_controller != null && _controller!.value.isInitialized) {
+      final XFile photo = await _controller!.takePicture();
+      // Handle the captured photo...
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: const BackButton(color: Colors.white),
+        title: const Text(
+          'StyleSnap',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w100,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: _controller == null || !_controller!.value.isInitialized
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(
+              fit: StackFit.expand,
+              children: [
+                CameraPreview(_controller!),
+                Column(
+                  children: [
+                    SizedBox(
+                        height: AppBar().preferredSize.height +
+                            MediaQuery.of(context).padding.top),
+                    Center(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: 2,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF9747FF),
+                                borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(20)),
+                              ),
+                              child: TextButton(
+                                onPressed: () {
+                                  // Handle Casual button press
+                                },
+                                child: Text(
+                                  "Casual",
+                                  style: TextStyle(
+                                    fontFamily: "Rochester",
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 2), // Gap between buttons
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF9747FF),
+                                borderRadius: BorderRadius.horizontal(
+                                    right: Radius.circular(20)),
+                              ),
+                              child: TextButton(
+                                onPressed: () {
+                                  // Handle Formal button press
+                                },
+                                child: Text(
+                                  "Formal",
+                                  style: TextStyle(
+                                    fontFamily: "Rochester",
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                    Expanded(
+                        child:
+                            Container()), // Pushes the camera controls to the bottom
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              _isFlashOn ? Icons.flash_on : Icons.flash_off,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            onPressed: _toggleFlash,
+                          ),
+                          GestureDetector(
+                            onTap: _takePicture,
+                            child: Container(
+                              width: 60,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(color: Colors.white, width: 4),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(
+                              Icons.flip_camera_ios,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            onPressed: _toggleCamera,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+    );
+  }
+}
